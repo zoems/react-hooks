f209f354b0cdf81dba1926f2a14ea5a05fdfcdfe
Zoe Mawby Smart <zoemsmart@gmail.com>
2024-06-06 16:54:57 +0200

    add error

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,31 +2,60 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+
+import {
+  PokemonDataView,
+  PokemonForm,
+  PokemonInfoFallback,
+  fetchPokemon,
+} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
+  const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
+
+
+  React.useEffect(() => {
+    console.log('effect')
+    if (!pokemonName) {
+      return
+    }
+
+    setPokemon(null)
+    console.log({pokemonName})
+    console.log(pokemonName)
+    fetchPokemon(pokemonName).then(pokemonData => {
+      console.log('fetching')
+      setPokemon(pokemonData)
+    })
+    .catch((error) => setError(error))
+
+    return () => {
+      //cleanup
+    }
+  }, [pokemonName])
+
+  if (error) {
+    console.log('error')
+    return(
+    <div role="alert">
+      There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+    </div>)
+
+  }
+
   // 🐨 use React.useEffect where the callback should be called whenever the
   // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
   // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null.
-  // (This is to enable the loading state when switching between different pokemon.)
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
 
-  // 💣 remove this
-  return 'TODO'
+  return !pokemonName ? (
+    'Submit a pokemon'
+  ) : !pokemon ? (
+    <PokemonInfoFallback name={pokemonName} />
+  ) : (
+    <PokemonDataView pokemon={pokemon} />
+  )
 }
 
 function App() {
